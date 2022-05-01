a844352b0d425aa1cdf20b7c6a0f217a7e7c239f
Felix Tran <104712325+phuctr2203@users.noreply.github.com>
2022-05-02 02:51:24 +0700

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "EEET2482/COSC2082 ASSIGNMENT"
+            "\nVACATION HOUSE EXCHANGE APPLICATION"
+            "\n \nInstructor: Mr. Linh Tran "
+            "\nGroup: Group 18 "
+            "\ns3911244, Tran Hoang Phuc "
+            "\ns..., ... "
+            "\ns..., ... "
+            "\ns..., ... " << endl;
+
+
+    return 0;
+}
